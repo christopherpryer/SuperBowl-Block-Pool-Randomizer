f27f0d7184476dbcbaa89c1ba1091581da6c8ab1
christopherpryer <christophpryer@gmail.com>
2020-01-28 16:48:05 +0000

    print formatting

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     """print randomly generated axes for Matt"""
     team1 = [str(random.randint(0, 9)) for i in range(10)]
     team2 = [str(random.randint(0, 9)) for i in range(10)]
-    print(', '.join(team1))
+    print('  ' + ', '.join(team1))
     print('\n'.join(team2))
 
 
