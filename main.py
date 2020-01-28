bb361e323e1cd71ee626639f2dfa1ecc21caa056
christopherpryer <christophpryer@gmail.com>
2020-01-28 18:12:48 +0000

    implemented no replacement sampling

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import random
 
 def main():
     """print randomly generated axes for Matt"""
-    team1 = [str(random.randint(0, 9)) for i in range(10)]
-    team2 = [str(random.randint(0, 9)) for i in range(10)]
+    team1 = [str(i) for i in random.sample(list(range(0, 10)), 10)]
+    team2 = [str(i) for i in random.sample(list(range(0, 10)), 10)]
     print('  ' + ', '.join(team1))
     print('\n'.join(team2))
 
